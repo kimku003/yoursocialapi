90be2d04a30256d5492e91d3d1707d99faa0bb8f
kimku003 <kimku679@gmail.com>
2025-06-20 15:47:15 +0200

    fix middleware problem for deployemnt

--- yoursocial/settings.py
+++ yoursocial/settings.py
@@ -69,15 +69,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',          # 1er - Sécurité globale
-    'whitenoise.middleware.WhiteNoiseMiddleware',            # 2e - Fichiers statiques
-    'django.contrib.sessions.middleware.SessionMiddleware',   # 3e - Sessions (DOIT être avant auth)
-    'corsheaders.middleware.CorsMiddleware',                 # 4e - CORS (juste après sessions)
-    'django.middleware.common.CommonMiddleware',             # 5e - URL standardisées
-    'django.middleware.csrf.CsrfViewMiddleware',            # 6e - Protection CSRF
-    'django.contrib.auth.middleware.AuthenticationMiddleware',  # 7e - Authentification
-    'django.contrib.messages.middleware.MessageMiddleware',  # 8e - Messages
-    'django.middleware.clickjacking.XFrameOptionsMiddleware', # 9e - Sécurité X-Frame
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Pour les fichiers statiques
+    'django.contrib.sessions.middleware.SessionMiddleware',  # DOIT être présent
+    'corsheaders.middleware.CorsMiddleware',  # Si vous utilisez CORS
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',  # Nécessaire pour l'admin
+    'django.contrib.messages.middleware.MessageMiddleware',  # Nécessaire pour l'admin
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'yoursocial.urls'
